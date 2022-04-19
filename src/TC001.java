53bb5644fe5ea3f58a6bc32ad260825ac8c7cca7
AlexaSky <alexasky111@gmail.com>
2022-04-19 17:45:30 -0400

    TC001 added

---
+++ src/TC001.java
@@ -0,0 +1,8 @@
+public class TC001 {
+    //working directory->local repo->remote
+    //red bc i don't want to track this file
+    //when I click add -> goes to Staging area and becomes Green
+    //add and commit is the same
+    //red means it is in a working directory
+    //green in staging area
+}
